dbcd80c8ae1d9e7ba45fc7738def3d203ddb2a81
Jeff Mitsou <shinetachi88@gmail.com>
2021-09-16 22:00:21 -0400

    worked on Dice Class

--- src/com/company/Dice.java
+++ src/com/company/Dice.java
@@ -1,4 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Dice {
+
+    private int sides;
+    private int value;
+
+    public Dice() {
+        this.sides = sides;
+        this.value = getValue();
+    }
+
+    public ArrayList<Integer> rollDie(int rolls) {
+        int currentRoll = 0;
+        ArrayList<Integer> rollScores = new ArrayList<Integer>();
+
+        while (currentRoll < rolls){
+            value = (int) (Math.random() * 1 + 1);
+            rollScores.add(value);
+            currentRoll++;
+            //System.out.println(value);
+        }
+        return rollScores;
+
+    }
+
+    public int getValue() {
+        return value;
+    }
 }

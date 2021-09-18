c009eab72ce10b4a3c982710c31cd38deb0f21ed
Jeff Mitsou <shinetachi88@gmail.com>
2021-09-17 21:41:44 -0400

    worked on Character class

--- src/com/company/Character.java
+++ src/com/company/Character.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Character {
+
+    public ArrayList<Character> player;
+
+    private String name;
+    private int hitPoints;
+
+    public Character(String name, int hp) {
+        this.name = name;
+        this.hitPoints = hp;
+    }
+
+
 }
